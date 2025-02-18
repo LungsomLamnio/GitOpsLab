19c98e60878fae03f13d2f1579805b2f3669db3b
Lungsom Lamnio <lungsomlamnio@gmail.com>
2025-02-18 14:24:49 +0530

    fix bugs

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,10 @@
 let btn = document.getElementById("btn");
 
+function printMessage() {
+  let mssg = document.getElementById("mssg");
+  mssg.innerText = "Hello";
+}
+
 btn.addEventListener("click", () => {
-  console.log("Hello");
+  printMessage();
 });
